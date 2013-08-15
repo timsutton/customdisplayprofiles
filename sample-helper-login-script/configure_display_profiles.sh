96475cb3da4c67684ee7a2de45abde866680c7ce
Timothy Sutton <tim@synthist.net>
2013-08-15 16:21:03 -0400

    Update configure_display_profiles.sh

--- sample-helper-login-script/configure_display_profiles.sh
+++ sample-helper-login-script/configure_display_profiles.sh
@@ -4,9 +4,9 @@
 #
 # Very simple helper script to run the customdisplayprofiles tool to
 # set profiles stored in a known folder location, with subfolders named
-# by display index, like in the sample structure below. No special
-# handling is done for the case of multiple profiles in a folder,
-# but the tool will ignore all arguments after the first anyway.
+# by display index, like in the sample structure below. The icc file itself
+# is given only by a shell wildcard, but the tool will only take the first
+# argument.
 #
 # This would allow someone calibrating a display to configure a profile
 # for all users simply by copying the profile to the correct folder
